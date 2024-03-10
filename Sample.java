9dbd5b74985d892e59c2c40673af5b1857cfbe2f
SHALINIMOURALY <126889011+SHALINIMOURALY@users.noreply.github.com>
2024-03-10 21:54:37 +0530

    basis

---
+++ Sample.java
@@ -0,0 +1,16 @@
+package Day1;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("by shalu");
+		int a,b,c;
+		a=100;
+		b=20;
+		c=a+b;
+		System.out.println("addition of two number:"+ c);
+
+	}
+
+}
